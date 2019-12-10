f446bd5653d858bd750c95e9970b29113d093268
Your Name <you@example.com>
2019-12-10 06:18:18 +0200

    nmongoDb now on Atlas cloud

---
+++ src/MongoDB_datasource/e-tendering-database.datasource.ts
@@ -0,0 +1,14 @@
+import {inject} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+import * as config from './e-tendering-database.datasource.json';
+
+export class ETenderingDatabaseDataSource extends juggler.DataSource {
+  static dataSourceName = 'eTenderingDatabase';
+
+  constructor(
+    @inject('datasources.config.eTenderingDatabase', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
